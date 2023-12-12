740ca08add17199d9fa945115a89223974e37079
0xhatsume <0xhatsume@gmail.com>
2023-12-12 23:02:03 +0800

    :poop: Bloop :)

--- src/TestToken.sol
+++ src/TestToken.sol
@@ -6,6 +6,6 @@ import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract TestToken is ERC20("TestToken", "TEST") {
     constructor() {
-        _mint(msg.sender, 1000*10**18);
+        _mint(msg.sender, 1000000*10**18);
     }
 }
